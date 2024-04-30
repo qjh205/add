ea77fe6bf612f378d003637475f9981d13307d47
unknown <2531064992@qq.com>
2024-04-30 11:30:14 +0800

    create number

---
+++ vue-project/src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>123</h1>
+    123
+    <h1>456</h1>
+    <h1>789</h1>
+    <h1>101112</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
